381bc38bd2fae0368504c19bd0c5d85b6cb21bde
aqeel-7 <aqeelpatel07@gmail.com>
2022-03-05 14:40:21 +0530

    Moved Youtube Exercises one directory up.

--- Youtube_Exs/01_hello.py
+++ Youtube_Exs/01_hello.py
@@ -1,11 +1,11 @@
 #Author: Shalu
 
-'''print('''Twinkle, twinkle, little star,
+print('''Twinkle, twinkle, little star,
 How I wonder what you are!
 Up above the world so high,
 Like a diamond in the sky.
 When the blazing sun is gone,
 When he nothing shines upon,
 Then you show your little light,
-Twinkle, twinkle, all the night.''')'''
+Twinkle, twinkle, all the night.''')
 print(5*1, 5*2, 5*3, 5*4, 5*5, 5*6, 5*7, 5*8, 5*9, 5*10)
